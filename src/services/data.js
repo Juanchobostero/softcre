483cdb7668e747a8314f5ce0a91bbb8967649cf8
juancruz <juancruzmart93@gmail.com>
2023-10-26 07:41:24 -0300

    Implements section projects

---
+++ src/services/data.js
@@ -0,0 +1,64 @@
+const projects = [
+    {
+        "id": 123,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "Olalá"
+    },
+    {
+        "id": 1234,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    },
+    {
+        "id": 12345,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    },
+    {
+        "id": 12342,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    },
+    {
+        "id": 12343,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    },
+    {
+        "id": 12344,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    },
+    {
+        "id": 123466,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    },
+    {
+        "id": 12343,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    },
+    {
+        "id": 12347,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    },
+    {
+        "id": 12349,
+        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
+        "success": true,
+        "projectName": "NISSI JOYAS"
+    }
+  ]
+
+  export default projects
