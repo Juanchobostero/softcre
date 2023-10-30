75f28b3b63feed429732dd88282d4d0b2673ae96
juancruz <juancruzmart93@gmail.com>
2023-10-29 22:53:54 -0300

    implementa cards de proyectos y pagina de detalle de cada uno

--- src/services/data.js
+++ src/services/data.js
@@ -3,62 +3,51 @@ const projects = [
         "id": 123,
         "details": "JavaScript and JQuery: Interactive Front-End Web Development",
         "success": true,
-        "projectName": "Olalá"
+        "projectName": "Olalá",
+        "image": "/static/NISSI.jpeg"
     },
     {
         "id": 1234,
         "details": "JavaScript and JQuery: Interactive Front-End Web Development",
         "success": true,
-        "projectName": "NISSI JOYAS"
+        "projectName": "NISSI JOYAS",
+        "image": "/static/OLALA.jpeg"
     },
     {
         "id": 12345,
         "details": "JavaScript and JQuery: Interactive Front-End Web Development",
         "success": true,
-        "projectName": "NISSI JOYAS"
+        "projectName": "HIGHLIGHT HERRAMIENTAS",
+        "image": "/static/HL.jpeg"
     },
     {
         "id": 12342,
         "details": "JavaScript and JQuery: Interactive Front-End Web Development",
         "success": true,
-        "projectName": "NISSI JOYAS"
+        "projectName": "ACERCA",
+        "image": "/static/ACERCA.jpeg"
     },
     {
         "id": 12343,
         "details": "JavaScript and JQuery: Interactive Front-End Web Development",
         "success": true,
-        "projectName": "NISSI JOYAS"
+        "projectName": "I. M. Roubineau",
+        "image": "/static/ROUBINEAU.jpeg"
     },
     {
         "id": 12344,
         "details": "JavaScript and JQuery: Interactive Front-End Web Development",
         "success": true,
-        "projectName": "NISSI JOYAS"
+        "projectName": "TRESOLQA",
+        "image": "/static/TRESOLQA.jpeg"
     },
     {
         "id": 123466,
         "details": "JavaScript and JQuery: Interactive Front-End Web Development",
         "success": true,
-        "projectName": "NISSI JOYAS"
+        "projectName": "UNNE",
+        "image": "/static/UNNE.jpeg"
     },
-    {
-        "id": 12343,
-        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
-        "success": true,
-        "projectName": "NISSI JOYAS"
-    },
-    {
-        "id": 12347,
-        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
-        "success": true,
-        "projectName": "NISSI JOYAS"
-    },
-    {
-        "id": 12349,
-        "details": "JavaScript and JQuery: Interactive Front-End Web Development",
-        "success": true,
-        "projectName": "NISSI JOYAS"
-    }
   ]
 
   export default projects
